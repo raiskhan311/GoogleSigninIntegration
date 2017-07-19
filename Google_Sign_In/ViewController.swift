11441630ea815b669c156e2714f6e6394355eb6d
raiskhan311 <raiskhan311@gmail.com>
2017-07-19 23:57:40 +0530

    goggle sign In Integration completed on Swift 3

--- Google_Sign_In/ViewController.swift
+++ Google_Sign_In/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import GoogleSignIn
+import Google
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInUIDelegate,GIDSignInDelegate {
 
+    @IBOutlet weak var lbl_name: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a ni
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error != nil{
+            print(error ?? "some error")
+            return
+        }
+        lbl_name.text = user.profile.email
+        print(user.profile.email)
+        print(user.profile.imageURL(withDimension: 400))
+        print(user.userID)
+        print(user.authentication.idToken)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btn_logingoogle(_ sender: Any) {
+        var error:NSError?
+        
+        GGLContext.sharedInstance().configureWithError(&error)
+        
+        if error != nil{
+            print(error ?? "some error")
+            return
+        }
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        GIDSignIn.sharedInstance().signIn()
+//        GIDSignIn.sharedInstance().signInSilently()
+//        let googlesigninbutton = GIDSignInButton()
+//        googlesigninbutton.center = view.center
+//        view.addSubview(googlesigninbutton)
     }
-
-
 }
 
